d97a497b60fbc7894153239de313b9b5a3baa90e
hungryfoolishn <88585526+hungryfoolishn@users.noreply.github.com>
2024-01-19 12:20:07 +0800

    Delete 浙江 directory

--- 浙江/驾驶舱统计/整合2022年通报数据/test0725.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-now = datetime.datetime.now()
-date = datetime.datetime.strftime(now - datetime.timedelta(days=1), '%Y%m%d')  # 获取日期
-# 当前时间  必填
-start_time = '{}'.format(date)
-end_time = '{}'.format(date)
-print(end_time)
